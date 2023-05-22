0d7e59e965b5acf4c417e01bbe99c9b95538d8c5
Hélvio Filho <helviosvf@gmail.com>
2023-05-22 17:18:53 -0300

    Chore: added splash and icon

--- android/app/src/release/java/com/hsvf/sigtrans/ReactNativeFlipper.java
+++ android/app/src/release/java/com/hsvf/sigtrans/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package com.sigtrans;
+package com.hsvf.sigtrans;
 
 import android.content.Context;
 import com.facebook.react.ReactInstanceManager;

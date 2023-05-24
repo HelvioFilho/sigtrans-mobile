f0d45fa4609218f874e5db2b95a7caef2708b4d1
Hélvio Filho <helviosvf@gmail.com>
2023-05-24 10:10:45 -0300

    Chore: created default data

---
+++ src/utils/defaultData.ts
@@ -0,0 +1,41 @@
+export const RETENTIONPARK = [
+  { id: "1", name: "Tijuca", address: "Rua 1, 123" },
+  { id: "2", name: "Grajaú", address: "Rua 2, 123" },
+  { id: "3", name: "Niterói A", address: "Rua 3, 123" },
+  { id: "4", name: "Niterói B", address: "Rua 4, 123" },
+  { id: "5", name: "RJ-345", address: "Rua 5, 123" },
+];
+
+export const SPECIES = [
+  { id: "1", name: "Passageiro" },
+  { id: "2", name: "Especial" },
+  { id: "3", name: "Carga" },
+  { id: "4", name: "Misto" },
+];
+
+export const VEHICLETYPE = [
+  { id: "1", name: "Automóvel" },
+  { id: "2", name: "Utilitário" },
+  { id: "3", name: "Motocicleta" },
+  { id: "4", name: "Ônibus" },
+  { id: "5", name: "Caminhão" },
+];
+
+export const DAMAGE = [
+  { id: "1", name: "Quebrado" },
+  { id: "2", name: "Arranhado" },
+  { id: "3", name: "Amassado" },
+  { id: "4", name: "Moça" },
+  { id: "5", name: "Faltando" },
+  { id: "6", name: "Outro" },
+];
+
+export const ACCESSORY = [
+  { id: "1", name: "Retrovisor esquerdo" },
+  { id: "2", name: "Retrovisor direito" },
+  { id: "3", name: "Para-brisa" },
+  { id: "4", name: "Porta esquerda frente" },
+  { id: "5", name: "Porta direita frente" },
+  { id: "6", name: "Rodas" },
+  { id: "7", name: "Pneus" },
+];

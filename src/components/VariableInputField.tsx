c1b5e6685999aed9c2066ce152bbc95febf07f07
Hélvio Filho <helviosvf@gmail.com>
2023-05-26 11:32:16 -0300

    Feat: created variable input that expands according to the content

---
+++ src/components/VariableInputField.tsx
@@ -0,0 +1,48 @@
+import { TextInputProps, View } from "react-native";
+import { Control, Controller } from "react-hook-form";
+import { TextInput } from "react-native";
+
+type VariableInputFieldProps = TextInputProps & {
+  name: string;
+  control: Control<any>;
+  changedHeight: number;
+};
+
+export function VariableInputField({
+  name,
+  control,
+  changedHeight,
+  ...rest
+}: VariableInputFieldProps) {
+  const height = `${changedHeight}px`;
+  return (
+    <View className="w-full">
+      <View className={`flex-row justify-center my-2 h-[${height}]`}>
+        <Controller
+          control={control}
+          name={name}
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              className={`
+              bg-gray-100 
+              border-2 
+              border-gray-600 
+              h-[${height}]
+              rounded-md 
+              py-3 
+              pl-4
+              pr-4 
+              w-full 
+              font-regular 
+              text-input
+            `}
+              onChangeText={onChange}
+              value={value}
+              {...rest}
+            />
+          )}
+        />
+      </View>
+    </View>
+  );
+}

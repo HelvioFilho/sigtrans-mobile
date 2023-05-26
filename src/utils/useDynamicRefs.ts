a41e1031e8c0a382faad49e4de60abbe8099ce3e
Hélvio Filho <helviosvf@gmail.com>
2023-05-26 09:58:04 -0300

    Chore: changed useDynamicRefs to improve performance

--- src/utils/useDynamicRefs.ts
+++ src/utils/useDynamicRefs.ts
@@ -1,15 +1,18 @@
-import { RefObject, createRef, useRef } from "react";
+import { RefObject, createRef, useMemo, useRef } from "react";
 import { TextInput } from "react-native";
 
 export function useDynamicRefs(ids: string[]): {
   [key: string]: RefObject<TextInput>;
 } {
   const refs = useRef<{ [key: string]: RefObject<TextInput> }>({}).current;
-  ids.forEach((id) => {
-    if (!refs[id]) {
-      refs[id] = createRef<TextInput>();
-    }
-  });
+
+  useMemo(() => {
+    ids.forEach((id) => {
+      if (!refs[id]) {
+        refs[id] = createRef<TextInput>();
+      }
+    });
+  }, [ids]);
 
   return refs;
 }

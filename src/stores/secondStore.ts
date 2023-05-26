9b1662f58661734942cc7a5428b5687e83fa388b
Hélvio Filho <helviosvf@gmail.com>
2023-05-26 13:23:58 -0300

    Feat: created second data store

---
+++ src/stores/secondStore.ts
@@ -0,0 +1,31 @@
+import { AdditionalDataDTO } from "@/dtos/AdditionalData.DTO";
+import { LocationDTO } from "@/dtos/LocationDTO";
+import { SelectItemProps } from "@/screens/newScreens/SecondInformation";
+import { getStorage, saveStorage } from "@/utils/storageHelper";
+import { create } from "zustand";
+
+type SecondDataProps = AdditionalDataDTO &
+  LocationDTO & {
+    observations: string;
+    checkList: SelectItemProps[];
+  };
+
+type SecondStore = {
+  secondData: SecondDataProps;
+  setSecondData: (data: SecondDataProps) => void;
+};
+
+const KEY = "secondInformation";
+
+const response = getStorage(KEY);
+let initialData = response ? JSON.parse(response) : ({} as SecondDataProps);
+
+export const useSecondStore = create<SecondStore>((set) => ({
+  secondData: initialData,
+  setSecondData: (data) => {
+    set(() => {
+      saveStorage(KEY, JSON.stringify(data));
+      return { secondData: data };
+    });
+  },
+}));

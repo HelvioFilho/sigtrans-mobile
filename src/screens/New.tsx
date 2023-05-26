0ed580b90cdbc14e9b264f650b9c1a261c8b46e8
Hélvio Filho <helviosvf@gmail.com>
2023-05-26 11:28:49 -0300

    Feat: added change between pages

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,8 +1,25 @@
 import { SafeAreaView } from "react-native";
 import { FirstInformation } from "./newScreens/FirstInformation";
+import { useRoute } from "@react-navigation/native";
+import { SecondInformation } from "./newScreens/SecondInformation";
+type RouteParamsProps = {
+  id: number;
+};
 
 export function New() {
-  let currentPage = <FirstInformation />;
+  const route = useRoute();
+  const params = route?.params as RouteParamsProps;
+
+  const page = params?.id ? params.id : 0;
+  let currentPage;
+
+  switch (page) {
+    case 0:
+      currentPage = <FirstInformation />;
+      break;
+    case 1:
+      currentPage = <SecondInformation />;
+  }
   return (
     <SafeAreaView className="flex-1 bg-slate-50">{currentPage}</SafeAreaView>
   );

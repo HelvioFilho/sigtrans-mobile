c6f9bb2aafec4c3498535abb09c8692929d216ad
Hélvio Filho <helviosvf@gmail.com>
2023-05-26 10:55:28 -0300

    Chore: created location helper

---
+++ src/utils/locationHelper.ts
@@ -0,0 +1,30 @@
+import * as Location from "expo-location";
+
+type LocationProps = {
+  longitude: number;
+  latitude: number;
+};
+
+export async function getPermissions() {
+  let { status } = await Location.requestForegroundPermissionsAsync();
+  if (status !== "granted") {
+    console.log("A permissão de localização não foi concedida");
+  }
+}
+
+export async function getCurrentLocation() {
+  let location: Location.LocationObject =
+    await Location.getCurrentPositionAsync({});
+  return location;
+}
+
+export async function getReverseGeocodeAsync(
+  location: LocationProps
+): Promise<String> {
+  let data: Location.LocationGeocodedAddress[] =
+    await Location.reverseGeocodeAsync(location);
+
+  return data[0].street
+    ? data[0].street
+    : "Não foi possível encontrar o endereço";
+}

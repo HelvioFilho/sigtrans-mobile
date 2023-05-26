7b79321e15aea8b9a67ad8dbff3baef310ea14a7
Hélvio Filho <helviosvf@gmail.com>
2023-05-26 10:16:46 -0300

    Feat: created first data store

---
+++ src/stores/firstStore.ts
@@ -0,0 +1,30 @@
+import { create } from "zustand";
+import { getStorage, saveStorage } from "@/utils/storageHelper";
+
+import { VehicleDTO } from "@/dtos/VehicleDTO";
+import { TowTruckDTO } from "@/dtos/TowTruckDTO";
+import { RetentionParkDTO } from "@/dtos/RetentionParkDTO";
+
+type FirstDataProps = RetentionParkDTO & TowTruckDTO & VehicleDTO;
+
+type FirstStore = {
+  firstData: Partial<FirstDataProps>;
+  setFirstData: (data: Partial<FirstDataProps>) => void;
+};
+
+const KEY = "firstInformation";
+
+const response = getStorage(KEY);
+let initialData = response
+  ? JSON.parse(response)
+  : ({} as Partial<FirstDataProps>);
+
+export const useFirstStore = create<FirstStore>((set) => ({
+  firstData: initialData,
+  setFirstData: (data) => {
+    set(() => {
+      saveStorage(KEY, JSON.stringify(data));
+      return { firstData: data };
+    });
+  },
+}));

582ec4b2ff8952b819f04437bd23e7d6e9734ef7
Hélvio Filho <helviosvf@gmail.com>
2023-05-26 21:07:15 -0300

    Feat: added view props to be able to manipulate the style externally

--- src/components/TitleWrapper.tsx
+++ src/components/TitleWrapper.tsx
@@ -1,12 +1,12 @@
-import { Text, View } from "react-native";
+import { Text, View, ViewProps } from "react-native";
 
-type TitleWrapperProps = {
+type TitleWrapperProps = ViewProps & {
   title: string;
 };
 
-export function TitleWrapper({ title }: TitleWrapperProps) {
+export function TitleWrapper({ title, ...rest }: TitleWrapperProps) {
   return (
-    <View className="flex-row items-center">
+    <View className="flex-row items-center" {...rest}>
       <View className="flex-grow h-[2px] bg-gray-400" />
       <View className="border-2 border-gray-400 px-4 py-3 rounded">
         <Text className="font-bold text-subtitle">{title}</Text>

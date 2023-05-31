e44487613b89099d05bee9ad81c472c0d66d28fb
Hélvio Filho <helviosvf@gmail.com>
2023-05-30 23:58:07 -0300

    Feat: created the image upload to the backend and the download of document images that I didn't create

--- src/utils/imageHelper.ts
+++ src/utils/imageHelper.ts
@@ -4,6 +4,7 @@ import * as MediaLibrary from "expo-media-library";
 import uuid from "react-native-uuid";
 import { generateId } from "./storageHelper";
 import { ImageSelectedProps } from "@/screens/newScreens/Gallery";
+import { api } from "@/services/api";
 
 const documentId = generateId();
 const imageDefault = {
@@ -11,6 +12,39 @@ const imageDefault = {
   allowsEditing: true,
   quality: 0.5,
 };
+
+type ExternalImageProps = {
+  uri: string;
+};
+
+export async function uploadImageToDatabase(uris: string[]) {
+  try {
+    const promises: Promise<ExternalImageProps>[] = uris.map(async (uri) => {
+      const fileExtension = uri.split(".").pop();
+      const photoFile = {
+        name: `${uuid.v4()}.${fileExtension}`.toLocaleLowerCase(),
+        uri: uri,
+        type: `image/${fileExtension}`,
+      } as any;
+
+      const photoUploadForm = new FormData();
+      photoUploadForm.append("file", photoFile);
+      photoUploadForm.append("name", uuid.v4() as string);
+
+      const updatedResponse = await api.post("sigtrans", photoUploadForm, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      return updatedResponse.data;
+    });
+    const responses = await Promise.all(promises);
+    return responses;
+  } catch (error) {
+    console.log("Algo deu errado ao fazer upload da imagem");
+    console.log(error);
+  }
+}
 
 export async function checkMediaPermission() {
   try {
@@ -44,7 +78,7 @@ async function saveImageInFileSystem(
       const fileName = result.assets[0].uri.substring(
         result.assets[0].uri.lastIndexOf("/") + 1
       );
-      const newFileUri = `${FileSystem.documentDirectory}gallery/${fileName}`;
+      const newFileUri = `${FileSystem.documentDirectory}${fileName}`;
 
       await FileSystem.copyAsync({
         from: result.assets[0].uri,
@@ -107,5 +141,35 @@ export async function deleteImageFromGallery(url: string) {
     return true;
   } catch (error) {
     throw new Error("Algo deu errado ao excluir a imagem da galeria");
+  }
+}
+
+export async function saveExternalImagesToStorage(
+  images: ExternalImageProps[]
+) {
+  try {
+    const promises: Promise<ImageSelectedProps>[] = images.map(
+      async (image) => {
+        const fileName = image.uri.substring(
+          image.uri.lastIndexOf("/") + 1
+        ) as string;
+        const newFileUri = `${FileSystem.documentDirectory}${fileName}`;
+        console.log("new file", newFileUri);
+        await FileSystem.downloadAsync(image.uri, newFileUri);
+
+        const imageSelected: ImageSelectedProps = {
+          id: uuid.v4() as string,
+          name: fileName,
+          documentId,
+          uri: newFileUri,
+        };
+        return imageSelected;
+      }
+    );
+    const responses = await Promise.all(promises);
+    return responses;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Algo deu errado ao salvar a imagem");
   }
 }
